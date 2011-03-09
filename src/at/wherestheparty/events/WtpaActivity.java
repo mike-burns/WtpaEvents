2e292b58c7646a3a9c123cfe7a3940d6dc5b8044
Mike Burns <mburns@thoughtbot.com>
2011-03-09 18:29:07 -0500

    Show the Web page

--- src/at/wherestheparty/events/WtpaActivity.java
+++ src/at/wherestheparty/events/WtpaActivity.java
@@ -3,13 +3,49 @@ package at.wherestheparty.events;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class WtpaActivity extends Activity
-{
-    /** Called when the activity is first created. */
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+import android.webkit.HttpAuthHandler;
+
+public class WtpaActivity extends Activity {
+  private WebView page;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.main);
+
+    page().loadUrl("http://ejohn.org/wtpa/");
+    page().setWebViewClient(new WtpaWebViewClient());
+  }
+
+  @Override
+  public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_BACK && page().canGoBack()) {
+      page().goBack();
+      return true;
+    } else
+      return super.onKeyDown(keyCode, event);
+  }
+
+  private class WtpaWebViewClient extends WebViewClient {
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public boolean shouldOverrideUrlLoading(WebView v, String url) {
+      v.loadUrl(url);
+      return true;
     }
+
+    @Override
+    public void onReceivedHttpAuthRequest(WebView v, HttpAuthHandler handler, String host, String realm) {
+      handler.proceed(UsernamePassword.username, UsernamePassword.password);
+    }
+  }
+
+  private WebView page() {
+    if (this.page == null)
+      this.page = (WebView)findViewById(R.id.webpage);
+    return this.page;
+  }
 }
